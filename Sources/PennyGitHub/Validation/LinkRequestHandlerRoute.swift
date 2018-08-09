11ed6b9cfac10123f9074ac0f74f4d9170a3d0a5
Logan Wright <logan.william.wright@gmail.com>
2018-08-08 21:15:17 -0400

    tracers

--- Sources/PennyGitHub/Validation/LinkRequestHandlerRoute.swift
+++ Sources/PennyGitHub/Validation/LinkRequestHandlerRoute.swift
@@ -5,7 +5,9 @@ import Penny
 
 extension LinkRequestHandler {
     static func handle(_ req: Request) throws -> Future<AccountLinkRequest> {
+        print("rec'd req")
         let ghlr = try req.content.decode(GitHubLinkRequest.self)
+        print("made glhr: \(ghlr)")
         return ghlr.flatMap { ghlr in
             let handler = LinkRequestHandler(worker: req, ghlr: ghlr)
             return try handler.handle()
